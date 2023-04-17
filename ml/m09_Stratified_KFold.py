a3f3dfce5c2366eda8d91996241cad56da7064e6
dltjdgh0928 <dltjdgh0928@naver.com>
2023-04-17 17:39:35 +0900

    study

--- ml/m09_Stratified_KFold.py
+++ ml/m09_Stratified_KFold.py
@@ -1,6 +1,6 @@
 import numpy as np
 from sklearn.datasets import load_iris
-from sklearn.model_selection import cross_val_score, cross_val_predict, train_test_split, KFold
+from sklearn.model_selection import cross_val_score, cross_val_predict, train_test_split, KFold, StratifiedKFold
 from sklearn.metrics import accuracy_score
 from sklearn.svm import SVC
 from sklearn.ensemble import RandomForestClassifier
@@ -10,11 +10,12 @@ x, y = load_iris(return_X_y=True)
 x_train, x_test, y_train, y_test = train_test_split(x, y, shuffle=True, random_state=337, test_size=0.2)
 
 n_splits = 5
-kf = KFold(n_splits=n_splits, shuffle=True, random_state=337)
+# kf = KFold(n_splits=n_splits, shuffle=True, random_state=337)
+kf = StratifiedKFold(n_splits=n_splits, shuffle=True, random_state=337)
 
 # 2. 모델
-# model = SVC()
-model = RandomForestClassifier()
+model = SVC()
+# model = RandomForestClassifier()
 
 # 3, 4 컴파일, 훈련, 평가, 예측
 score = cross_val_score(model, x_train, y_train, cv=kf)
