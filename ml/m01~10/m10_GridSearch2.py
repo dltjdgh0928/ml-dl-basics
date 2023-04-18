750fbe851e40edbe228daf594fdd2ef60528069a
dltjdgh0928 <dltjdgh0928@naver.com>
2023-04-18 11:05:42 +0900

    study

--- ml/m01~10/m10_GridSearch2.py
+++ ml/m01~10/m10_GridSearch2.py
@@ -1,17 +1,33 @@
 import numpy as np
 from sklearn.datasets import load_iris
-from sklearn.model_selection import train_test_split, KFold, cross_val_score, GridSearchCV
+from sklearn.model_selection import train_test_split, KFold, cross_val_score, GridSearchCV, StratifiedKFold
 from sklearn.metrics import accuracy_score
 from sklearn.svm import SVC
+import time
 
 # 1. 데이터
 x, y = load_iris(return_X_y=True)
 
-x_train, x_test, y_train, y_test = train_test_split(x, y, shuffle=True, random_state=337, test_size=0.2, stratify=y)
+x_train, x_test, y_train, y_test = train_test_split(x, y, shuffle=True, random_state=337, test_size=0.2)
 
 n_splits = 5
 kf = KFold(n_splits=n_splits, shuffle=True, random_state=337)
+# kf = StratifiedKFold(n_splits=n_splits, shuffle=True, random_state=337)
 
 parameters = [
-    {'C':[1,10,100,1000], 'kernel':['linear'], 'degree':[3,4,5]}
+    {'C':[1,10,100,1000], 'kernel':['linear'], 'degree':[3,4,5]},
+    {'C':[1,10,100], 'kernel':['rbf', 'linear'], 'gamma':[0.001, 0.0001]},
+    {'C':[1,10,100,1000], 'kernel':['sigmoid'], 'gamma':[0.01, 0.001, 0.0001], 'degree':[3, 4]},
+    {'C':[0.1, 1], 'gamma':[1, 10]}
 ]
+
+# GridSearch CV default : StratifiedKFold 
+model = GridSearchCV(SVC(), parameters, cv=5, verbose=1, refit=True, n_jobs=-1)
+start_time = time.time()
+model.fit(x_train, y_train)
+end_time = time.time()
+
+print('best estimator : ', model.best_estimator_)
+print('best params : ', model.best_params_)
+print('best score : ', model.best_score_)
+print('model score : ', model.score(x_test, y_test))
