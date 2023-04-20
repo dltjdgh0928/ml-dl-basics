0c582277d994cb5b88de6f07d32d4652106e3122
dltjdgh0928 <dltjdgh0928@naver.com>
2023-04-20 14:23:23 +0900

    study

--- keras_prac_1/air4.py
+++ keras_prac_1/air4.py
@@ -53,3 +53,11 @@ date = datetime.datetime.now()
 date = date.strftime("%m%d_%H%M")
 
 submission.to_csv(save_path + date + 'submission.csv', index=False)
+
+import matplotlib.pyplot as plt
+import seaborn as sns
+print(test_data.corr())
+plt.figure(figsize=(10, 8))
+sns.set(font_scale=1.2)
+sns.heatmap(train_data.corr(), square=True, annot=True, cbar=True)
+plt.show()
