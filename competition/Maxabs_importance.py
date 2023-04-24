0d06d34c2402f3ad65d464f6138c4cfdf047f2cd
dltjdgh0928 <dltjdgh0928@naver.com>
2023-04-24 10:37:06 +0900

    study

--- competition/Maxabs_importance.py
+++ competition/Maxabs_importance.py
@@ -1,16 +1,17 @@
 import pandas as pd
 import numpy as np
 from sklearn.model_selection import train_test_split, KFold
-from sklearn.preprocessing import MinMaxScaler, LabelEncoder, MaxAbsScaler
+from sklearn.preprocessing import MinMaxScaler, LabelEncoder, MaxAbsScaler, QuantileTransformer
 from sklearn.metrics import mean_squared_error
 from sklearn.gaussian_process import GaussianProcessRegressor
 from sklearn.preprocessing import PolynomialFeatures
 import optuna
 import datetime
 import warnings
+from optuna.integration import SkoptSampler
 warnings.filterwarnings('ignore')
 
-poly = PolynomialFeatures(degree=2, include_bias=False)
+# poly = PolynomialFeatures(degree=2, include_bias=False)
 
 def RMSE(x, y):
     return np.sqrt(mean_squared_error(x, y))
@@ -69,39 +70,47 @@ test_csv = test_csv.drop(['Height(Feet)', 'Height(Remainder_Inches)'], axis=1)
 scaler = MaxAbsScaler()
 x = pd.DataFrame(scaler.fit_transform(x))
 test_csv = pd.DataFrame(scaler.transform(test_csv))
+for i in range(1000):
+    _, x_test, _, y_test = train_test_split(x, y, test_size=0.7, random_state=123, shuffle=True)
 
-for i in range(10000):
-    kf = KFold(n_splits=5, shuffle=True, random_state=i)
+    # optuna.logging.disable_default_handler()
+    # sampler = SkoptSampler(
+    #     skopt_kwargs={'n_random_starts':5,
+    #                   'acq_func':'EI',
+    #                   'acq_func_kwargs': {'xi':0.02}})
 
-    for train_idx, test_idx in kf.split(x):
-        x_train, x_test = x.iloc[train_idx], x.iloc[test_idx]
-        y_train, y_test = y.iloc[train_idx], y.iloc[test_idx]
+    # for i in range(10000):
+    #     kf = KFold(n_splits=8, shuffle=True, random_state=i+337)
+
+    # for train_idx, test_idx in kf.split(x):
+    #     x_train, x_test = x.iloc[train_idx], x.iloc[test_idx]
+    #     y_train, y_test = y.iloc[train_idx], y.iloc[test_idx]
 
     
-        def objective(trial):
-            alpha = trial.suggest_loguniform('alpha', 0.0000001, 0.1)
-            n_restarts_optimizer  = trial.suggest_int('n_restarts_optimizer', 1, 20)
-            optimizer = trial.suggest_categorical('optimizer', ['fmin_l_bfgs_b', 'Powell', 'CG'])
+    def objective(trial):
+        alpha = trial.suggest_loguniform('alpha', 0.0000001, 0.1)
+        n_restarts_optimizer  = trial.suggest_int('n_restarts_optimizer', 1, 20)
+        optimizer = trial.suggest_categorical('optimizer', ['fmin_l_bfgs_b', 'Powell', 'CG'])
 
-            model = GaussianProcessRegressor(
-                alpha=alpha,
-                n_restarts_optimizer=n_restarts_optimizer,
-                optimizer=optimizer,
-            )
-            
-            model.fit(x_train, y_train)
-            
-            print('GPR result : ', model.score(x_test, y_test))
-            
-            y_pred = np.round(model.predict(x_test))
-            rmse = RMSE(y_test, y_pred)
-            print('GPR RMSE : ', rmse)
-            if rmse < 0.2:
-                submit_csv['Calories_Burned'] = model.predict(test_csv)
-                date = datetime.datetime.now()
-                date = date.strftime('%m%d_%H%M%S')
-                submit_csv.to_csv(path_save + date + str(round(rmse, 5)) + '.csv')
-            return rmse
-        opt = optuna.create_study(direction='minimize')
-        opt.optimize(objective, n_trials=20)
-        print('best param : ', opt.best_params, 'best rmse : ', opt.best_value)
+        model = GaussianProcessRegressor(
+            alpha=alpha,
+            n_restarts_optimizer=n_restarts_optimizer,
+            optimizer=optimizer,
+        )
+        
+        model.fit(x, y)
+        
+        print('GPR result : ', model.score(x_test, y_test))
+        
+        y_pred = np.round(model.predict(x_test))
+        rmse = RMSE(y_test, y_pred)
+        print('GPR RMSE : ', rmse)
+        if rmse < 0.2:
+            submit_csv['Calories_Burned'] = np.round(model.predict(test_csv))
+            date = datetime.datetime.now()
+            date = date.strftime('%m%d_%H%M%S')
+            submit_csv.to_csv(path_save + date + str(round(rmse, 5)) + '.csv')
+        return rmse
+    opt = optuna.create_study(direction='minimize')
+    opt.optimize(objective, n_trials=20)
+    print('best param : ', opt.best_params, 'best rmse : ', opt.best_value)
