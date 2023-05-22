65782d212a71c871ba519e44cce6483a683151a3
dltjdgh0928 <dltjdgh0928@naver.com>
2023-05-22 16:22:05 +0900

    study

---
+++ keras/keras61 ~ 70/keras68_GlobalAveragePooling.py
@@ -0,0 +1,56 @@
+from tensorflow.keras.datasets import mnist
+from tensorflow.python.keras.models import Sequential
+from tensorflow.python.keras.layers import Dense, Conv2D, Flatten, MaxPooling2D, GlobalAveragePooling2D
+from sklearn.preprocessing import MinMaxScaler, StandardScaler, MaxAbsScaler, RobustScaler
+from tensorflow.python.keras.callbacks import EarlyStopping
+import numpy as np
+from sklearn.metrics import accuracy_score
+import pandas as pd
+import tensorflow as tf
+from tensorflow.keras.utils import to_categorical
+import time
+
+tf.random.set_seed(337)
+
+# 1. 데이터
+(x_train, y_train), (x_test, y_test) = mnist.load_data()
+
+x_train = x_train.reshape(60000, 28, 28, 1)
+x_test = x_test.reshape(10000, 28, 28, 1)
+
+y_train = to_categorical(y_train)
+y_test = to_categorical(y_test)
+
+# 2. 모델구성
+model = Sequential()
+model.add(Conv2D(64, (2,2), padding='same', input_shape=(28,28,1)))
+model.add(MaxPooling2D())
+model.add(Conv2D(64, (2,2), padding='valid', activation='relu'))
+model.add(Conv2D(32, 2))
+# model.add(Flatten())
+model.add(GlobalAveragePooling2D())
+model.add(Dense(10, activation='softmax'))
+model.summary()
+
+# 3. 컴파일, 훈련
+model.compile(loss='categorical_crossentropy', optimizer='adam', metrics='acc')
+es = EarlyStopping(monitor='val_acc', mode='min', patience=100, verbose=1, restore_best_weights=True)
+
+start = time.time()
+hist = model.fit(x_train, y_train, epochs=30, batch_size=128)
+end = time.time()
+
+# 4. 평가, 예측
+result = model.evaluate(x_test, y_test)
+print('result : ', result)
+print('loss :', result[0])
+print('acc', result[1])
+y_predict = model.predict(x_test)
+acc = accuracy_score(np.argmax(y_test,axis=1), np.argmax(y_predict,axis=1))
+print(f'acc : {acc}')
+print(f'time used : {int((end - start)//60)}m {round((end - start)%60, 3)}s')
+
+# import matplotlib.pyplot as plt 
+
+# plt.plot(hist.history['val_acc'], label='val_acc')
+# plt.show()
