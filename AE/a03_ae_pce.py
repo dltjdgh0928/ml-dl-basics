30704c0af50129d7b33fadcf10b9926ffe666aa7
dltjdgh0928 <dltjdgh0928@naver.com>
2023-05-30 15:15:17 +0900

    study

--- AE/a03_ae_pce.py
+++ AE/a03_ae_pce.py
@@ -1,7 +1,11 @@
 import numpy as np
+import tensorflow as tf
 from tensorflow.keras.datasets import mnist
 from tensorflow.keras.models import Sequential, Model
 from tensorflow.keras.layers import Dense, Input
+seed=337
+tf.random.set_seed(seed)
+np.random.seed(seed)
 
 # 1. 데이터
 (x_train, _), (x_test, _) = mnist.load_data()
@@ -9,8 +13,8 @@ from tensorflow.keras.layers import Dense, Input
 x_train = x_train.reshape(60000, 784).astype('float32')/255.
 x_test = x_test.reshape(10000, 784).astype('float32')/255.
 
-x_train_noised = x_train + np.random.normal(0, 1, size=x_train.shape)
-x_test_noised = x_test + np.random.normal(0, 1, size=x_test.shape)
+x_train_noised = x_train + np.random.normal(0, 2, size=x_train.shape)
+x_test_noised = x_test + np.random.normal(0, 2, size=x_test.shape)
 
 print(np.random.normal(0, 1, size=x_train.shape))
 
@@ -29,7 +33,17 @@ def autoencoder(hidden_layer_size):
     model.add(Dense(784, activation='sigmoid'))
     return model
 
-model = autoencoder(1)
+
+model = autoencoder(713)        # 100
+# model = autoencoder(486)        # 99.9
+# model = autoencoder(331)        # 99%
+# model = autoencoder(154)        # pca 95% 
+
+# print(np.argmax(cumsum >= 0.95)+1)        # 154
+# print(np.argmax(cumsum >= 0.99)+1)        # 331
+# print(np.argmax(cumsum >= 0.999)+1)        # 486
+# print(np.argmax(cumsum >= 1.0)+1)        # 713
+
 model.compile(optimizer='adam', loss='mse')
 model.fit(x_train_noised, x_train, epochs=30, batch_size=128, validation_split=0.2)
 
